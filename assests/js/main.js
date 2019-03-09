d2a45c08766e6ca3368310d9fe4df4d893065f9d
jad <jadelramadi@gmail.com>
2019-03-09 11:50:39 -0500

    fixed bug with script tags

---
+++ assests/js/main.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyBwJVYMdbP96XJLiDZxcxVcWKVK1wCtFSk",
+    authDomain: "project-1-21c94.firebaseapp.com",
+    databaseURL: "https://project-1-21c94.firebaseio.com",
+    projectId: "project-1-21c94",
+    storageBucket: "project-1-21c94.appspot.com",
+    messagingSenderId: "423051883696"
+  };
+  firebase.initializeApp(config);
