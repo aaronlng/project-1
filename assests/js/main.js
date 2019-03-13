8cd628d507346e0a5e61e711c35b7ab9c8a9cda8
jad <jadelramadi@gmail.com>
2019-03-12 20:33:06 -0400

    added ign api and tested it

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,9 +1,19 @@
 var config = {
-    apiKey: "AIzaSyBwJVYMdbP96XJLiDZxcxVcWKVK1wCtFSk",
-    authDomain: "project-1-21c94.firebaseapp.com",
-    databaseURL: "https://project-1-21c94.firebaseio.com",
-    projectId: "project-1-21c94",
-    storageBucket: "project-1-21c94.appspot.com",
-    messagingSenderId: "423051883696"
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyBwJVYMdbP96XJLiDZxcxVcWKVK1wCtFSk",
+  authDomain: "project-1-21c94.firebaseapp.com",
+  databaseURL: "https://project-1-21c94.firebaseio.com",
+  projectId: "project-1-21c94",
+  storageBucket: "project-1-21c94.appspot.com",
+  messagingSenderId: "423051883696"
+};
+firebase.initializeApp(config);
+
+var NewsURL = "https://newsapi.org/v2/everything?q=" + trending + "&language=en&apiKey=3f407cd0f52f4f3c819efcd2e3b147c9";
+var trending = "games+xbox+pc+playstation+nintendo";
+$.ajax({
+  url: NewsURL,
+  method: "GET"
+}).then(function (data) {
+  console.log(data);
+});
+
